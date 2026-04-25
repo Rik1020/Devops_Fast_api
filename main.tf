1e428521a6f21e0da1d573df59162b8d0d762f5d
Supriyo Karmakar <spriyo1002@gmail.com>
2026-04-25 18:05:19 +0530

    Uploaded to git

--- main.tf
+++ main.tf
@@ -83,16 +83,52 @@ resource "aws_instance" "ec2" {
 
   user_data = <<-EOF
               #!/bin/bash
+
+              # Update system
               apt-get update -y
+
+              # Install Docker
               apt-get install docker.io -y
               systemctl start docker
               systemctl enable docker
 
+              # Install Python & pip
               apt-get install python3-pip -y
+
+              # Install FastAPI & Uvicorn
               pip3 install fastapi uvicorn
+
+              # Move to ec2 user home
+              cd /home/ubuntu
+
+              # Create FastAPI app
+              cat <<EOT > main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI running via Terraform  - SUPRIYO 🚀"}
+
+@app.get("/health")
+def health():
+    return {"status": "OK"}
+
+@app.get("/hello/{name}")
+def greet(name: str):
+    return {"message": f"Hello {name}"}
+EOT
+
+              # Run app in background
+              nohup uvicorn main:app --host 0.0.0.0 --port 8000 > app.log 2>&1 &
               EOF
 
   tags = {
     Name = "devops-ec2"
   }
+  
+}
+output "app_url" {
+  value = "http://${aws_instance.ec2.public_ip}:8000"
 }
